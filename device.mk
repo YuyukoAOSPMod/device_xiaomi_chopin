ec34b9b4730c0e9bb9f14d29cddc2157b2132bca
Sushrut1101 <guptasushrut@gmail.com>
2023-08-24 10:08:07 +0800

    chopin: Build boot HAL

--- device.mk
+++ device.mk
@@ -67,6 +67,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-service \
     android.hardware.boot@1.1-mtkimpl.recovery \
     android.hardware.boot@1.1-mtkimpl
 
