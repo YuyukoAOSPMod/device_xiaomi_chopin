c100820ee95c7d3bbaf3742410b2b00403601e6f
Sushrut1101 <guptasushrut@gmail.com>
2023-08-24 13:08:50 +0800

    chopin: Build USB HAL from source

--- device.mk
+++ device.mk
@@ -408,6 +408,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.1.vendor
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/config/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi/)
