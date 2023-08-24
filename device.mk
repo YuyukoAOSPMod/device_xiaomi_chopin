ac62724da3df579790e76b0c07723bfb64b9b529
SamarV-121 <samarvispute121@pm.me>
2023-08-24 15:43:50 +0800

    chopin: Link older libutils for PQ HAL * Abort message:  'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'

--- device.mk
+++ device.mk
@@ -460,6 +460,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libutils-v32
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy.chopin \
