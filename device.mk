41a3cf5516ef29d53354b71c74dce2821fe797b2
Vaisakh Murali <mvaisakh@statixos.com>
2023-08-24 12:25:07 +0800

    chopin: Switch to AOSP OSS Audio HALs Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org> Change-Id: Ic9c0d344915585391a73ef7ed755b37ed4f43803 Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -50,17 +50,19 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
     android.hardware.audio.common-util.vendor \
-    android.hardware.audio.common@5.0.vendor \
     android.hardware.audio.common@6.0-util.vendor \
     android.hardware.audio.common@6.0.vendor \
     android.hardware.audio.common@7.0-enums.vendor \
     android.hardware.audio.common@7.0-util.vendor \
-    android.hardware.audio.effect@7.0.vendor \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@6.0.vendor \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio@7.0-util.vendor \
     android.hardware.audio@7.0.vendor \
-    android.hardware.audio.effect@7.0-impl:32
+    android.hardware.audio.effect@7.0.vendor \
+    android.hardware.audio.effect@7.0-impl
 
 PRODUCT_PACKAGES += \
     audio.primary.default \
